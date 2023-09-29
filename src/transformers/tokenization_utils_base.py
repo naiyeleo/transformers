f28b44bfd5d6ba94714d146e23db3d5e2aa834c8
Yingqi Liu <naiyeleo@gmail.com>
2023-09-28 21:43:21 -0700

    Update tokenization_utils_base.py

--- src/transformers/tokenization_utils_base.py
+++ src/transformers/tokenization_utils_base.py
@@ -2961,7 +2961,7 @@ class PreTrainedTokenizerBase(SpecialTokensMixin, PushToHubMixin):
         if padding_strategy == PaddingStrategy.LONGEST:
             max_length = max(len(inputs) for inputs in required_input)
             padding_strategy = PaddingStrategy.MAX_LENGTH
-         warnings.warn(f"max length, {max_length}, {padding_strategy}, batch_size, {batch_size}," )
+        warnings.warn(f"max length, {max_length}, {padding_strategy}, batch_size, {batch_size},")
         
         batch_outputs = {}
         for i in range(batch_size):
@@ -2978,7 +2978,7 @@ class PreTrainedTokenizerBase(SpecialTokensMixin, PushToHubMixin):
                 if key not in batch_outputs:
                     batch_outputs[key] = []
                 batch_outputs[key].append(value)
-             warnings.warn(f"outputs, {[(k, len(v)) for k, v in outputs.items()]}")
+            warnings.warn(f"outputs, {[(k, len(v)) for k, v in outputs.items()]}")
             
         return BatchEncoding(batch_outputs, tensor_type=return_tensors)
 
